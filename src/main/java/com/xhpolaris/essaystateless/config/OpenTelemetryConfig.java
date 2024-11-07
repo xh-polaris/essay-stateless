ab6a57921df6856b784d0a7e7eaaad649f996abf
univero <1449610641@qq.com>
2024-11-07 09:17:24 +0800

    feat: 添加了OpenTelemetry

---
+++ src/main/java/com/xhpolaris/essaystateless/config/OpenTelemetryConfig.java
@@ -0,0 +1,15 @@
+package com.xhpolaris.essaystateless.config;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenTelemetryConfig {
+
+    @Bean
+    OpenTelemetry getOpenTelemetry() {
+        return GlobalOpenTelemetry.get();
+    }
+}
