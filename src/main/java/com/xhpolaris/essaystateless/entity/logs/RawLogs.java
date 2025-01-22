16e3bc476b75db160ff4e44dfad9a90f9550f77e
univero <1449610641@qq.com>
2025-01-22 12:32:01 +0800

    fix: 解决了logs被忽略没push的问题

---
+++ src/main/java/com/xhpolaris/essaystateless/entity/logs/RawLogs.java
@@ -0,0 +1,18 @@
+package com.xhpolaris.essaystateless.entity.logs;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "raw_logs")
+@Data
+public class RawLogs {
+    @Id
+    private String id;
+    private String url;
+    private String request;
+    private String response;
+    private LocalDateTime createTime;
+}
