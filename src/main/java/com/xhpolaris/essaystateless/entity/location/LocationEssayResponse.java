a978f915d0e258a1f3c11ff47074414a798742c9
Zephyrtoria <2594379136@qq.com>
2025-02-17 23:02:21 +0800

    feat: 实现作文范围定位API、作文段落定位API的处理值返回

--- src/main/java/com/xhpolaris/essaystateless/entity/location/LocationEssayResponse.java
+++ src/main/java/com/xhpolaris/essaystateless/entity/location/LocationEssayResponse.java
@@ -18,18 +18,3 @@ public class LocationEssayResponse {
     private String message;
 }
 
-@Data
-class EssayBox {
-    /**
-     * 定位后高度
-     */
-    private double height;
-    /**
-     * 定位，四个坐标依次是左上下x1,y1，右下x2,y2
-     */
-    private double[][] location;
-    /**
-     * 定位后宽度
-     */
-    private double width;
-}
